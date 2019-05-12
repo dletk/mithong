85b429fe50e08cfdff51f3306bad2e5f53878d6b
dletk <duc.mtle@gmail.com>
2019-05-12 11:46:43 -0500

    Added required option for some fields and isAdmin for User

--- models/User.js
+++ models/User.js
@@ -2,15 +2,20 @@
 
 // Load mongoose 
 const mongoose = require("mongoose");
-const {Schema} = mongoose.Schema;
+const { Schema } = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: String,
-    lastname: String,
-    firstname: String,
+    username: { type: String, required: true },
+    lastname: { type: String, required: true },
+    firstname: { type: String, required: true },
     dateOfBirth: Date,
-    email: String,
-    joinedDate: {type: String, default: Date.now}
+    email: { type: String, required: true },
+    joinedDate: { type: String, default: Date.now },
+    // This is for password
+    hash: String,
+    salt: String,
+    // Administration option
+    isAdmin: { type: Boolean, default: false},
 });
 
 mongoose.model("User", userSchema);
