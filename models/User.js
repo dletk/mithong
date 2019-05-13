cbd7a42481af28746a20e6433c60a605341829f5
dletk <duc.mtle@gmail.com>
2019-05-12 23:26:04 -0500

    Refactored User and added some validations

--- models/User.js
+++ models/User.js
@@ -5,18 +5,42 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: { type: String, required: true },
-    lastname: { type: String, required: true },
-    firstname: { type: String, required: true },
+    username: { 
+        type: String, 
+        required: true,
+        minlength: 3, // Make sure the username is long enough to be descriptive
+        maxlength: 25
+    },
+    lastname: { 
+        type: String, 
+        required: true 
+    },
+    firstname: { 
+        type: String, 
+        required: true 
+    },
+    email: { 
+        type: String, 
+        required: true 
+    },
     dateOfBirth: Date,
-    email: { type: String, required: true },
-    profilePic: { data: Buffer, contentType: String},
-    joinedDate: { type: String, default: Date.now },
+    // Reference: https://stackoverflow.com/questions/29780733/store-an-image-in-mongodb-using-node-js-express-and-mongoose
+    profilePic: { 
+        data: Buffer, 
+        contentType: String
+    },
+    joinedDate: { 
+        type: String, 
+        default: Date.now 
+    },
     // This is for password
     hash: String,
     salt: String,
     // Administration option
-    isAdmin: { type: Boolean, default: false},
+    isAdmin: { 
+        type: Boolean, 
+        default: false
+    },
     // Status option
     isOnline: Boolean,
 });
